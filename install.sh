c8292e21f6f5a5587c5505b21ab05f7739f78e0c
n_nagami <n_nagami@clickmaker.jp>
2022-08-01 16:46:08 +0900

    Update

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 SCRIPT_DIR=$(cd $(dirname $0);pwd)
 cd $HOME
 ln -snf $SCRIPT_DIR/.gitconfig .gitconfig
-ln -snf $SCRIPT_DIR/.minttyrc  .minttyrc
 ln -snf $SCRIPT_DIR/.tmux.conf .tmux.conf
 ln -snf $SCRIPT_DIR/.vimrc     .vimrc
+ln -snf $SCRIPT_DIR/.zshrc     .zshrc
+ln -snf $SCRIPT_DIR/.p10k.zsh  .p10k.zsh
